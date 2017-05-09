d29a81d2a7fe9fbefe07ad49c4439a40b74be18d
Krishna Vootla <vootla.krishna@gmail.com>
2017-05-09 10:49:42 -0400

    commit 54756

--- main.tf
+++ main.tf
@@ -25,20 +25,10 @@ variable "environment" {
   default     = ""
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-
 
 /**
- * Security Groups/SSH
+ * Security Groups/Graylog
  */
-
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region     = "${var.aws_region}"
-  }
    
 resource "aws_security_group" "main_security_group" {
     name   = "${format("%s-%s-%s", var.organization, var.environment, var.sg_type)}"
